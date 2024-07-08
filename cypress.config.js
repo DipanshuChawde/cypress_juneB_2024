0c63613e832497fbe58c8418ce41a216206b81d3
DipanshuChawde <dipanshuchoude@gmail.com>
2024-07-08 08:56:10 +0530

    8july

--- cypress.config.js
+++ cypress.config.js
@@ -7,20 +7,55 @@ module.exports = defineConfig({
       // implement node event listeners here
 
       //task1 (file cyTask.cy.js)
-      on('task', {
+      // on('task', {
+      //   print() {
+      //     console.log('welcome')
+      //     return null
+      //   }
+      // })
+      // //------------------------------
+      // //task2 (file cyTask.cy.js)
+      // on('task', {
+      //   myLog(msg) {
+      //     console.log(msg)
+      //     return null
+      //   }
+      // })
+      // //--------------------
+      // //task3 (file cyTask.cy.js)
+      // on('task', {
+      //   addition({ a, b }) {
+      //     console.log(a + b)
+      //     return a + b
+      //   }
+      // })
+      // //-------------------------
+
+      //task1 (file cyTask.cy.js)
+      //on('task',taskname(){funcn def})
+      on('task',{
+        print(){
+          console.log('i am learning cypress')
+          return null
+
+      }})
+      //---------------------------task2--------
+      on('task',{
         myLog(msg) {
           console.log(msg)
           return null
+
         }
       })
-      //--------------------
-      //task2 (file cyTask.cy.js)
+      //----------------task3----------------
       on('task',{
-        addition({a,b}) {
+        addition({a,b}){
           console.log(a+b)
           return a+b
         }
       })
+ 
+
 
     },
   },
