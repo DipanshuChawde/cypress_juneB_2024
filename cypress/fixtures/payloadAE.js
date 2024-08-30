33c103304325e06b0a5834990fa991cea9fd6ae2
DipanshuChawde <dipanshuchoude@gmail.com>
2024-08-30 09:20:45 +0530

    30Aug

--- cypress/fixtures/payloadAE.js
+++ cypress/fixtures/payloadAE.js
@@ -1,4 +1,5 @@
-export default{
+export default[
+{
     name : "Dipanshu",
     email : `dipanshu${Math.floor(Math.random()*1000)+1}@gmail.com`,
     gender : 'Mrs',
@@ -6,6 +7,15 @@ export default{
     dayB : 26,
     month : 'November',
     year : "2008",
-
-
+    fn : 'dipanshu',
+    ln : 'chawde',
+    comny : 'infosys',
+    add1 : 'pune1',
+    add2 : 'pune2',
+    country :'India',
+    state : 'Maharashtra',
+    ZipCode : '411048',
+    mobno : 9987654323,
+    city : 'pune'
 }
+]
